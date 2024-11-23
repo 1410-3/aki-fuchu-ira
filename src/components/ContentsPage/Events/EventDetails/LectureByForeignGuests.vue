ebce5abc3b7a37a90a1776da2fed0d30b86fc561
1410-3 <60973430+1410-3@users.noreply.github.com>
2024-11-23 10:45:47 +0900

    update ・Added event details page ・Change folder name ・Unified UI for details

---
+++ src/components/ContentsPage/Events/EventDetails/LectureByForeignGuests.vue
@@ -0,0 +1,53 @@
+<template>
+  <v-app>
+    <v-container>
+      <v-row 
+        justify="center" 
+        class="mb-6">
+        <v-col 
+          cols="12" 
+          md="8">
+          <h1 class="text-center font-weight-bold">
+            令和6年度2回目 外国人ゲストによる講演会 
+          </h1>
+          <p class="text-center">
+            お待たせしました、令和6年度2回目 外国人ゲストによる講演会を実施いたします！！！
+          </p>
+        </v-col>
+      </v-row>
+
+      <v-row justify="center">
+        <v-col 
+          cols="12" 
+          md="8">
+          <v-card outlined>
+            <v-card-title class="font-weight-bold">
+              内容
+            </v-card-title>
+            <v-card-text>
+              近日公開予定です、お楽しみに！
+            </v-card-text>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: "LectureByForeignGuests",
+};
+</script>
+
+<style scoped>
+.text-center {
+  text-align: center;
+}
+.mb-6 {
+  margin-bottom: 24px;
+}
+.mt-6 {
+  margin-top: 24px;
+}
+</style>
---
+++ src/pages/eventDetails/lectureByForeignGuests.vue
@@ -0,0 +1,9 @@
+<template>
+  <Header /> 
+  <LectureByForeignGuests />
+</template> 
+
+<script setup>
+import Header from '@/components/Header/Header.vue';
+import LectureByForeignGuests from '@/components/ContentsPage/Events/EventDetails/LectureByForeignGuests.vue';
+</script>
