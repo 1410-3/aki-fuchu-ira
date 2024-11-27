40a6468f48c39a5cbaabea7f81c9ce8579307611
1410-3 <60973430+1410-3@users.noreply.github.com>
2024-11-27 10:45:37 +0900

    change top page contents

--- src/components/TopPage/Contents.vue
+++ src/components/TopPage/Contents.vue
@@ -1,63 +1,26 @@
 <template>
   <v-container class="fill-height">
-    <v-responsive class="align-centerfill-height mx-auto" max-width="900">
+    <v-responsive class="align-center fill-height mx-auto" max-width="900">
       <div class="text-center">
         <h1 class="text-h2">
           Welcome To Fuchu <br>
           International Relations Association!!
         </h1>
+        <v-card outlined>
+          <v-img :src=path />
+        </v-card>
       </div>
-
-      <div class="py-4" />
-
-      <v-row>
-        <v-col cols="6">
-          <v-card append-icon="mdi-open-in-new" class="py-4" color="surface-variant" rounded="lg"
-            title="交流協会について / About the relations association" variant="text" to="/overview" tag="router-link">
-            <v-overlay opacity=".06" scrim="primary" contained model-value persistent />
-          </v-card>
-        </v-col>
-
-        <v-col cols="6">
-          <v-card append-icon="mdi-open-in-new" class="py-4" color="surface-variant" rounded="lg" title="イベント / event"
-            variant="text" to="/events" tag="router-link">
-            <v-overlay opacity=".06" scrim="primary" contained model-value persistent />
-          </v-card>
-        </v-col>
-
-        <v-col cols="6">
-          <v-card append-icon="mdi-open-in-new" class="py-4" color="surface-variant"
-            href="https://www.instagram.com/international.fuchu/" rel="noopener noreferrer" rounded="lg" target="_blank"
-            title="インスタグラム / Instagram" variant="text">
-            <v-overlay opacity=".06" scrim="primary" contained model-value persistent />
-          </v-card>
-        </v-col>
-
-        <v-col cols="6">
-          <v-card append-icon="mdi-open-in-new" class="py-4" color="surface-variant" rounded="lg"
-            title="会員募集 / Recruitment of members" variant="text" to="/membership" tag="router-link">
-            <v-overlay opacity=".06" scrim="primary" contained model-value persistent />
-          </v-card>
-        </v-col>
-
-        <v-col cols="6">
-          <v-card append-icon="mdi-open-in-new" class="py-4" color="surface-variant" rounded="lg"
-            title="語学教室 / language class" variant="text" to="/class" tag="router-link">
-            <v-overlay opacity=".06" scrim="primary" contained model-value persistent />
-          </v-card>
-        </v-col>
-
-        <v-col cols="6">
-          <v-card append-icon="mdi-open-in-new" class="py-4" color="surface-variant" rounded="lg"
-            title="アーカイブ / archive" variant="text" to="/archive" tag="router-link">
-            <v-overlay opacity=".06" scrim="primary" contained model-value persistent />
-          </v-card>
-        </v-col>
-      </v-row>
     </v-responsive>
   </v-container>
 </template>
 
-<script setup>
-//
+<script>
+export default {
+  name: "ContentsPage",
+  data() {
+    return {
+      path: new URL('@/assets/worldPiece.webp', import.meta.url).href
+    };
+  }
+}
 </script>
